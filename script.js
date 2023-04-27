6c13a7d3fccc5220d47fb829cf98519b35c34b10
LinhLuu254 <llinh254@gmail.com>
2023-04-27 14:18:16 -0400

    content added

--- script.js
+++ script.js
@@ -1,52 +1,28 @@
 
+let stateData;
+
 const process = (data) => {
         
-const info = data[0].states;
+let  info = data[0].states;
+
+stateData = info; 
 
 console.log(data[0].states);
 
 info.forEach((state) => {
-    let allStates=" ";
+    
 
-    let addState =
-    `<div class="col m-2 p-4"><h2><i class="bi bi-heart-arrow"></i>${state.name}</h2>`;
-    allStates +=addState
+    let stateOption = `<option>${state.name}</option>`;
+    
 
-    console.log(addState);
-    $(".row").append(allStates);
-
-    //style
-    $("h2").css("color","#A0522D")
-            .css("font-family","fantasy");
-
-    $("h5").css("color","brown");
-
-    $("h6").css("color","#008B8B")
-
-    $(".tag").css("font-size", "22px")
-            .css("font-style","italic")
-            .css("font-family","cursive")
-            .css("color","#008080")
-            .css("text-align","center");
-
-    $("span").css("color","brown");
-
-    $("li").css("list-style-type","1F44D");
-
-    $(".col").css("background-color","#F4A460")
-            .css("opacity","0.8")
-            .css("border", "dashed 3px black")
-            .css("border-radius", "5px");
-    $("img").css("weight", "100px")
-            .css("height","80px")
-            .css("position","relative")
-            .css("margin-left","40%");
+    console.log(stateOption);
+    $("#statesList").append(stateOption);
 });
 };
+
 const getData = () => {
 $.ajax({
     type: "GET",
-    // ""
     url:"https://adv-web-us-state-final-500.uc.r.appspot.com/api/v1/states",
     dataType: "json",
     success: function (result, status, xhr) {
@@ -67,4 +43,79 @@ $.ajax({
 });
 };
 
-$(document).ready(getData);
+const setUp = () =>{
+        let listBox = $("<fieldset><legend>Choose the state you want to read:</legend><select id='statesList' name='statesList' Size=25></fieldset>");
+        $("#lists").append(listBox);
+
+        getData();
+
+        $("#popup").fadeOut();
+
+        listBox.on("click", function () {
+
+                // when click happens, get the index of the item selected
+                let index = $("select[name='statesList'] option:selected").index();
+                console.log("index is", index);
+             
+                $("#popup").fadeIn();
+
+                let name = `${stateData[index].name}`;
+                let container = document.getElementById('name');
+                container.replaceChildren();
+                $("#name").append(name);
+
+                let nickname= ` <ul>
+                        <li><span class="nickname">Nickname:</span> ${stateData[index].nickname}</li>
+                        <li>Statehood: ${stateData[index].date_of_statehood}</li>
+                        </ul>`;
+                let container0 = document.getElementById('nickname');
+                container0.replaceChildren();
+                $("#nickname").append(nickname);
+
+                let motto = `<span class="motto">"${stateData[index].motto}"</span>`;
+                let container1 = document.getElementById('motto');
+                container1.replaceChildren();
+                $("#motto").append(motto);
+
+                let stateImg = document.createElement("img");
+                stateImg.src = stateData[index].image;
+                let container2 = document.getElementById('imgFlag');
+                container2.replaceChildren();
+                $("#imgFlag").append(stateImg);
+
+                let info =`<ul>
+                        <li>Captital: ${stateData[index].capital}</li>
+                        <li>Landmass: ${stateData[index].landmass}</li>
+                        <li>Population: ${stateData[index].population}</li>
+                        </ul>
+                        <h4>Major industry</h4>
+                        <p>${stateData[index].majorindustry}</p>`;
+
+                let container3 = document.getElementById('info');
+                container3.replaceChildren();
+                $("#info").append(info);
+
+                let symbol = `<h4>Symbols</h4>
+                        <ul>
+                        <li><span class="bird">Bird:</span> ${stateData[index].symbols[0].value}</li>
+                        <li><span class="flower">Flower:</span> ${stateData[index].symbols[1].value}</li>
+                        <li><span class="tree">Trees:</span> ${stateData[index].symbols[2].value}</li>
+                        </ul>`;
+                let container4 = document.getElementById('symbols');
+                container4.replaceChildren();
+                $("#symbols").append(symbol);
+
+                let tourist = `
+                <h4>Tourist Attraction</h4>
+                <p>${stateData[index].touristattractions}</p>`;
+
+                let container5 = document.getElementById('tourist');
+                container5.replaceChildren();
+                $("#tourist").append(tourist);
+                
+                
+              }); 
+              
+};
+
+$(document).ready(setUp);
